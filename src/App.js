cd2c6cc6ac111b09ebdd219ce5924be750ccf18a
Wan Ahmad Ikhwannaufal <139196434+ikhwannaufal@users.noreply.github.com>
2024-02-16 12:35:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [records, setRecords] = useState([])
 
   useEffect(() => {
-    fetch('Data.json')
+    fetch('https://phenomenal-clafoutis-123631.netlify.app/Data.json')
     .then(res => res.json())
     .then(data => {
       setColumn(Object.keys(data.users[0]))
